a00edb48f686e7c648ea7f20fcfff160ba95f8f8
Patrycja Komenda <patrycjakomenda8@gmail.com>
2022-11-25 05:44:15 +0100

    Add js to browser sync

---
+++ js/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function titleClickHandler(event){
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!');
+
+  /* [DONE] remove class 'active' from all article links  */
+
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  const links = document.querySelectorAll('titles a');
+  links.add('active')
+  console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleSelector');
+  /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector('href')
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
