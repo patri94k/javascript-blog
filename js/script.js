1e1a8459fef0b5c63bc1fe4ef1ad7d5fe254dd9a
Patrycja Komenda <patrycjakomenda8@gmail.com>
2022-12-06 08:58:14 +0100

    Dokończenie funkcji clickhandler

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
-
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('clickedElement (with plus):' + clickedElement);
 }
@@ -11,7 +11,7 @@ const activeLinks = document.querySelectorAll('.titles a.active');
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 };
-/*[IN PROGRESS] add class 'active' to the clicked link*/
+/*[DONE] add class 'active' to the clicked link*/
 clickedElement.classList.add('active');
 
 console.log('clickedElement:', clickedElement);
@@ -22,9 +22,15 @@ const activeArticles = document.querySelectorAll('.post');
 for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 };
-/*get 'href' attribute form the clicked link*/
+/*get 'href' attribute from the clicked link*/
+const articleSelector = clickedElement.getAttribute('href');
+console.log(articleSelector);
+
 /*find the correct article using the selector (value of 'href' attribute)*/
+const targetArticle = document.querySelector('article-1')
 /*add class 'active' to the correct article*/
+targetArticle.classList.add('active');
+
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
