ff6caea730d309e4eecfb87042283b9f9ae4b80c
Patrycja Komenda <patrycjakomenda8@gmail.com>
2022-12-06 07:04:57 +0100

    Remove active class from articles

--- js/script.js
+++ js/script.js
@@ -5,8 +5,19 @@ function titleClickHandler(event){
   console.log(event);
 }
 /*remove class 'active' from all articles links*/
+
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+};
 /*add class 'active' to the clicked link*/
 /*remove class 'active' from all articles*/
+const activeArticles = document.querySelectorAll('.post');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+};
 /*get 'href' attribute form the clicked link*/
 /*find the correct article using the selector (value of 'href' attribute)*/
 /*add class 'active' to the correct article*/
