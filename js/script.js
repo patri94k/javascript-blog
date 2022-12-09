1f14aaf8dedd50cd9b25e50370288923ee0dde06
Patrycja Komenda <patrycjakomenda8@gmail.com>
2022-12-09 08:36:47 +0100

    Fix bug

--- js/script.js
+++ js/script.js
@@ -32,8 +32,55 @@ function titleClickHandler(event){
     /* add class 'active' to the correct article */
     targetArticle.classList.add('active');
   }
+ 
+
+//5.4 Generowanie listy tytułów
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+  optArticleTagsSelector = '.post-tags .list';
+
+function generateTitleLinks(){
+  /*remove contents of titleList*/
+  const titleList = document.querySelector(optTitleSelector);
+  function clearMessages(){
+    document.getElementById('titles').innerHTML = '';
+  }
+  /*for each article*/
+  const articles = document.querySelector(optArticleSelector);
+
+  let html = '';
+  for(let article of articles){
+    console.log(article);
+
+    /*get the article id*/
+    const articleId = article.getAttribute('id');
+    /*find the title element*/
+    const title = querySelector(optTitleSelector).innerHTML;
+    /*get the title from the title element*/
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    /*create HTML of the link*/
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+    /*insert link onto titlelist*/
+    titleList.innerHTML = titleList.innerHTML + linkHTML;
+    titleList.insertAdjacentHTML('beforebegin', linkHTML);
+    html = html + linkHTML;
+  }
+  titleList.innerHTML = html;
   const links = document.querySelectorAll('.titles a');
-  
+  console.log(links);
+
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+}
+const links = document.querySelectorAll('.titles a');
+console.log(links);
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
+
+generateTitleLinks();
+
